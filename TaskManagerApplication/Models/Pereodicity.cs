2f28d223a90f348b2ddcee5963d3aebb92907d1a
Владислав Горохов <gbg.0101@mail.ru>
2019-06-13 18:16:40 +0600

    Добавлено несколько классов

---
+++ TaskManagerApplication/Models/Pereodicity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerApplication.Models
+{
+    public enum Pereodicity
+    {
+        OneTime,
+        OnceADay,
+        OnceAWeek,
+        OnceAMounth,
+        OnceAYear,
+    }
+}
---
+++ TaskManagerApplication/Models/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerApplication.Models
+{
+    public enum Type
+    {
+        Email,
+        Download,
+        MoveDirectiory
+    }
+}
